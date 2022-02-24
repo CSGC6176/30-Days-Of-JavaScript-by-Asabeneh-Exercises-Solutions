ebd00b5fac1dd53ef23e24f4ef837a8bbccdc6c6
CSGC6176 <govind36889@gmail.com>
2022-02-24 18:51:59 +0530

    changes

--- Day6.js
+++ Day6.js
@@ -1,63 +1,63 @@
-// for(let i=100;i>0;i--){
-//     console.log(i)
-// }
+for(let i=100;i>0;i--){
+    console.log(i)
+}
 
 for(let i = 7;i<=20;i++){
     console.log(`${i} * ${i}  ${i * i}`)
 }
 
-// const countries=['Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
-// const newArr=[]
-// for(let i=0;i<countries.length;i++){
-//     newArr.push(countries[i].toUpperCase())
-// }
-// console.log(newArr)
+const countries=['Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
+const newArr=[]
+for(let i=0;i<countries.length;i++){
+    newArr.push(countries[i].toUpperCase())
+}
+console.log(newArr)
 
-//SUm of number
+SUm of number
 
-// const numbers=[1,2,3,4,5]
-// let sum =0;
-// for(let i=0;i<numbers.length;i++){
-//     sum=sum+numbers[i]
-// }
-// console.log('Sum of numbers'+sum)
+const numbers=[1,2,3,4,5]
+let sum =0;
+for(let i=0;i<numbers.length;i++){
+    sum=sum+numbers[i]
+}
+console.log('Sum of numbers'+sum)
 
-// const numbers=[1,2,3,4,5]
-// const newArr=[]
-// for(let i=0;i<numbers.length;i++){
-//     newArr.push(numbers[i]**2)
-// }
-// console.log(newArr)
+const numbers=[1,2,3,4,5]
+const newArr=[]
+for(let i=0;i<numbers.length;i++){
+    newArr.push(numbers[i]**2)
+}
+console.log(newArr)
 
-//while loop
-// let i=0
-// while(i<=5){
-//     console.log(i)
-//     i++
-// }
+while loop
+let i=0
+while(i<=5){
+    console.log(i)
+    i++
+}
 
-//Do-while loop
-// let i=0;
-// do{
-//     console.log(i)
-//     i++
-// }
-// while(i<=5)
-
-
-//For of loop
-// for (const element of arr) {
-//     // code goes here
-//   }
+Do-while loop
+let i=0;
+do{
+    console.log(i)
+    i++
+}
+while(i<=5)
 
 
-// let sum=0
-//   const numbers = [1, 2, 3, 4, 5]
-//   for (const num of numbers) {
-//         console.log(num)      
-//         console.log(num * num )   
-//         console.log(sum=sum+num)   
-//   }
+For of loop
+for (const element of arr) {
+    // code goes here
+  }
+
+
+let sum=0
+  const numbers = [1, 2, 3, 4, 5]
+  for (const num of numbers) {
+        console.log(num)      
+        console.log(num * num )   
+        console.log(sum=sum+num)   
+  }
 
 const webTechs = [
     'HTML',
